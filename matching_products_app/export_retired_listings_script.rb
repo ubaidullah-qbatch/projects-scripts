0f2de46e41ce1010152b56acfbaf820ee1cc33e7
ubaidullah7822 <ubaidullah@qbatch.com>
2022-03-14 14:59:49 +0500

    export account id as well in csv

--- matching_products_app/export_retired_listings_script.rb
+++ matching_products_app/export_retired_listings_script.rb
@@ -21,7 +21,7 @@ ap_ids = h.keys.uniq
 # va_id
 # listing_source user/shared/system listings
 
-attributes = ["SKU", "UPC", "Supplier Url", "Marketplace Url", "Verified Match", "Is Default", "Quantity Multiplier", "Item Id", "Listed Price", "Supplier Price", "Supplier Stock", "Supplier Shipping", "Variation", "Is Mismatch", "Ma Price", "Va Id", "Listing Source", "Error Type", "Error Log"]
+attributes = ["Account Id", "SKU", "UPC", "Supplier Url", "Marketplace Url", "Verified Match", "Is Default", "Quantity Multiplier", "Item Id", "Listed Price", "Supplier Price", "Supplier Stock", "Supplier Shipping", "Variation", "Is Mismatch", "Ma Price", "Va Id", "Listing Source", "Error Type", "Error Log"]
 @platform = Platform.find_by(name: 'walmart')
 listings_klass, suppliers_klass = Platform.set_listing_and_supplier(@platform)
 listings_table_name = listings_klass.constantize.table_name
@@ -31,11 +31,11 @@ csv_data = CSV.generate(headers: true) do |csv|
   csv << attributes
   ap_ids.in_groups_of(1000, false).each_with_index do |ids_batch, index|
     puts "iteration: #{index}"
-    listings_json = JSON.parse(AccountsProduct.where(id: ids_batch).joins(:suppliers).select("#{listings_table_name}.id, #{listings_table_name}.sku, #{listings_table_name}.upc, #{listings_table_name}.auto_ordering_enabled, #{suppliers_table_name}.quantity_multiplier, #{listings_table_name}.item_id, #{listings_table_name}.is_mismatch, #{suppliers_table_name}.name, #{suppliers_table_name}.url, #{suppliers_table_name}.is_default, #{suppliers_table_name}.price, #{listings_table_name}.last_submitted_price, #{suppliers_table_name}.manual_price, #{suppliers_table_name}.price_lock, #{suppliers_table_name}.stock, #{listings_table_name}.last_submitted_stock, #{listings_table_name}.va_id, #{suppliers_table_name}.manual_stock, #{suppliers_table_name}.stock_lock, #{suppliers_table_name}.shipping_fee, #{suppliers_table_name}.platform_id, #{suppliers_table_name}.variation, #{listings_table_name}.created_at as created_at, #{suppliers_table_name}.refreshed_at as refreshed_at, #{Supplier::OFFRER_STATUS_MAPPING}, #{listings_table_name}.is_mismatch, #{listings_table_name}.va_id, #{listings_table_name}.ma_price, #{listings_table_name}.listing_source, #{listings_table_name}.error_type, #{listings_table_name}.error_log").to_json)
+    listings_json = JSON.parse(AccountsProduct.where(id: ids_batch).joins(:suppliers).select("#{listings_table_name}.account_id, #{listings_table_name}.id, #{listings_table_name}.sku, #{listings_table_name}.upc, #{listings_table_name}.auto_ordering_enabled, #{suppliers_table_name}.quantity_multiplier, #{listings_table_name}.item_id, #{listings_table_name}.is_mismatch, #{suppliers_table_name}.name, #{suppliers_table_name}.url, #{suppliers_table_name}.is_default, #{suppliers_table_name}.price, #{listings_table_name}.last_submitted_price, #{suppliers_table_name}.manual_price, #{suppliers_table_name}.price_lock, #{suppliers_table_name}.stock, #{listings_table_name}.last_submitted_stock, #{listings_table_name}.va_id, #{suppliers_table_name}.manual_stock, #{suppliers_table_name}.stock_lock, #{suppliers_table_name}.shipping_fee, #{suppliers_table_name}.platform_id, #{suppliers_table_name}.variation, #{listings_table_name}.created_at as created_at, #{suppliers_table_name}.refreshed_at as refreshed_at, #{Supplier::OFFRER_STATUS_MAPPING}, #{listings_table_name}.is_mismatch, #{listings_table_name}.va_id, #{listings_table_name}.ma_price, #{listings_table_name}.listing_source, #{listings_table_name}.error_type, #{listings_table_name}.error_log").to_json)
     listings_json.each do |listing|
       auto_ordering_enabled = listing['auto_ordering_enabled'] ? 1 : 0
       is_default = listing['is_default'] ? 1 : 0
-      csv << [listing['sku'], listing['upc'], listing['url'], make_marketplace_url(listing['item_id']), 
+      csv << [listing['account_id'], listing['sku'], listing['upc'], listing['url'], make_marketplace_url(listing['item_id']), 
       auto_ordering_enabled, is_default, listing['quantity_multiplier'], listing['item_id'], 
       listing['last_submitted_price'], listing['price'], listing['stock'], listing['shipping_fee'], 
       listing['variation'], listing['is_mismatch'], listing['ma_price'], listing['va_id'], listing['listing_source'],
