cd4313e916eaea14f40c691017376d5cb29b0f51
ubaidullah7822 <ubaidullah@qbatch.com>
2022-05-31 14:16:19 +0500

    updates in restricted brands script to get users pecentage litings that might get retired if we copy inventory restricted brands to all users

--- matching_products_app/restricted_brand_listings_stats.rb
+++ matching_products_app/restricted_brand_listings_stats.rb
@@ -12,18 +12,31 @@ tail -f rake.out
 cat rake.out
 : > rake.out
 
-
 pid = 10905
 
 ps aux | grep run_brand_stats_script
 ps aux | grep 10905
 
+arr = JSON.parse(UserBrand.where(marketplace: 'walmart', inventory_restricted: true).joins(:brand).distinct.select("restricted_reason, name").to_json).each{|row| row.delete('id')}
+arr.uniq!{|row| row['name']};nil
+CSV.open("public/inventory_restricted_brands.csv", "w") do |csv|
+  csv << arr.first.keys.map(&:to_s).map(&:humanize).map(&:upcase)
+  arr.each do |r|
+    csv << r.values
+  end;nil
+end;nil
+csv = CSV.parse(File.read('public/inventory_restricted_brands.csv'), headers: true)
+# File.open("public/inventory_restricted_brands.csv", 'w') {|file| file.write(arr.to_json)}
+
+
+
 task run_brand_stats_script: [:environment] do
-  User.find(3).switch!
-  system_brands = PlatformBrand.where(source: :system, platform_id: Platform.find_by(name: 'walmart').id).joins(:brand).pluck(:name).uniq
+  arr = JSON.parse(UserBrand.where(marketplace: 'walmart', inventory_restricted: true).joins(:brand).distinct.select("restricted_reason, name").to_json).each{|row| row.delete('id')}
+  system_brands = arr.map{|row| row['name']}
 
   arr = []
-  User.active.where.not(id: 3).each_with_index do |user, index|
+  User.active.each_with_index do |user, index|
+    puts "email: #{user.email} -- index: #{index}"
     Apartment::Tenant.switch(user.tenant_name) do
       ids = Account.walmart.enabled.valid.active.ids
       c = 0
@@ -31,17 +44,26 @@ task run_brand_stats_script: [:environment] do
         c += listings_batch.where("brand IN (?)", system_brands).count
         # puts "listings_might_retire: #{c}"
       end
-      hash = {email: user.email, total_active_listings: AccountsProduct.uploaded.where(account_id: ids).count, total_active_listings_with_brands: AccountsProduct.uploaded.where(account_id: ids).where.not(brand: [nil, '']).count, active_listings_might_retire: c}
-      puts hash
-      arr << hash
+      total_active_listings = AccountsProduct.uploaded.where(account_id: ids).count
+      total_active_listings_with_brands = AccountsProduct.uploaded.where(account_id: ids).where.not(brand: [nil, '']).count
+      if c.positive?
+        hash = {email: user.email, total_active_listings: total_active_listings, total_active_listings_with_brands: total_active_listings_with_brands, active_listings_might_retire: c}
+        puts hash
+        arr << hash
+      end
     end
+    puts "arr count: #{arr.count}"
   end
-  File.open("public/listings_might_retire.json", 'w') {|file| file.write(arr.to_json)}
+  File.open("public/listings_might_retire_with_inventory_restricted_brands.json", 'w') {|file| file.write(arr.to_json)}
 end
 
 user = User.find_by(email: "alexandradirect@heonboard.com")
 user.switch!
 
-arr1 = JSON.parse(File.read("public/listings_might_retire.json"))
+arr1 = JSON.parse(File.read("/Users/apple/Downloads/listings_might_retire_with_inventory_restricted_brands.json"))
+arr1.sort_by{|a| a['active_listings_might_retire']}.reverse
 
 pp arr1.select{|a| a['active_listings_might_retire'].positive?}
+
+
+File.open("public/listings_might_retire_with_inventory_restricted_brands.json", 'w') {|file| file.write(arr1.to_json)}
